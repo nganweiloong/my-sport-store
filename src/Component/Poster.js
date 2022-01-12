c7474214d55daa81e16e48790788a0654b5283b6
Wei Loong <nganweiloong@gmail.com>
2022-01-12 23:54:36 +0800

    updated background image and text

---
+++ src/Component/Poster.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Poster() {
+  return (
+    <div className="poster-container">
+      <article>
+        <div className="info">
+          <div className="content">
+            <h2>Welcome to WLSE</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue
+              quisque egestas diam in arcu cursus euismod quis. Tristique magna
+              sit amet purus gravida quis blandit.
+            </p>
+            <button className="btn-shop">Shop Now!</button>
+          </div>
+          <div className="feature-info"></div>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export default Poster;
--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./Component/Nav";
+import Poster from "./Component/Poster";
 
 function MainComponent() {
   return (
@@ -7,6 +8,9 @@ function MainComponent() {
       <header>
         <Nav />
       </header>
+      <main>
+        <Poster />{" "}
+      </main>
     </div>
   );
 }
