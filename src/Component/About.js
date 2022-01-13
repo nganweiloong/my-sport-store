f6a2e30779b98fb53c1f7fff46bb69ac0da658f9
Wei Loong <nganweiloong@gmail.com>
2022-01-13 20:52:46 +0800

    added about.us info content

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,9 +1,19 @@
 import React from "react";
+import AboutUs from "./AboutUs";
 
 function About() {
   return (
-    <div className="container-background-2">
-      <div className="about-container"></div>
+    <div className="container-backgrodund-2">
+      <div className="about-container fixed-container">
+        <div className="about-header">
+          <h1>ABOUT US</h1>
+          <div className="about-subheader">
+            <h2 className="about-subheader-active">ABOUT US</h2>
+            <h2>OUR HISTORY</h2>
+          </div>
+        </div>
+        <AboutUs />
+      </div>
     </div>
   );
 }
---
+++ src/Component/AboutUs.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container ">
+      <div className="img-wrapper">
+        <img src="/images/about.jpg"></img>
+      </div>
+      <div className="about-us-info">
+        <h2>Our Story</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra
+          orci sagittis eu volutpat odio. Ut venenatis tellus in metus vulputate
+          eu scelerisque. Et pharetra pharetra massa massa ultricies mi quis.
+          Vitae congue mauris rhoncus aenean vel elit scelerisque. Diam donec
+          adipiscing tristique risus nec. Ac auctor augue mauris augue neque.
+          Non sodales neque sodales ut etiam sit.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
