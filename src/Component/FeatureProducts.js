6cc052197d1ff0800246d198994ce1c4373d5089
Wei Loong <nganweiloong@gmail.com>
2022-01-13 14:08:50 +0800

    updated poster auto slide

---
+++ src/Component/FeatureProducts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FeatureProducts() {
+  return (
+    <div className="feature-products-container">
+      <div className="feature-products">
+        <h2 className="title">Featured Products</h2>
+      </div>
+    </div>
+  );
+}
+
+export default FeatureProducts;
