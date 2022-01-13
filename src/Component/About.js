4272b03b0d1795f7cf6f9b1d6c603d969917648b
Wei Loong <nganweiloong@gmail.com>
2022-01-13 21:30:58 +0800

    added ourhistory in about page

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,18 +1,22 @@
 import React from "react";
 import AboutUs from "./AboutUs";
+import { Link } from "react-router-dom";
+import OurHistory from "./OurHistory";
 
 function About() {
   return (
     <div className="container-backgrodund-2">
       <div className="about-container fixed-container">
         <div className="about-header">
-          <h1>ABOUT US</h1>
+          <h1>ABOUT</h1>
           <div className="about-subheader">
             <h2 className="about-subheader-active">ABOUT US</h2>
+
             <h2>OUR HISTORY</h2>
           </div>
         </div>
         <AboutUs />
+        <OurHistory />
       </div>
     </div>
   );
---
+++ src/Component/OurHistory.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function OurHistory() {
+  return (
+    <div className="our-history-container ">
+      <div className="img-wrapper">
+        <img src="/images/ourhistory.jpg"></img>
+      </div>
+      <div className="about-us-info">
+        <h2>Our History</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra
+          orci sagittis eu volutpat odio. Ut venenatis tellus in metus vulputate
+          eu scelerisque. Et pharetra pharetra massa massa ultricies mi quis.
+          Vitae congue mauris rhoncus aenean vel elit scelerisque. Diam donec
+          adipiscing tristique risus nec. Ac auctor augue mauris augue neque.
+          Non sodales neque sodales ut etiam sit.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default OurHistory;
