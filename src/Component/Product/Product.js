689d9543530096db2c66df57a31a495491881565
Wei Loong <nganweiloong@gmail.com>
2022-01-15 13:17:52 +0800

    added product item and details

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAdjust,
@@ -7,6 +7,7 @@ import {
   faSearch,
   faSlidersH,
 } from "@fortawesome/free-solid-svg-icons";
+import StarRatings from "react-star-ratings";
 
 function Product() {
   return (
@@ -24,15 +25,50 @@ function Product() {
             <FontAwesomeIcon icon={faSlidersH} />
             <span>Filter</span>
           </button>
-
-          <div className="product-grid">
-            <div className="box box1">box1</div>
-            <div className="box box2">box2</div>
-          </div>
         </form>
+        <div className="product-grid">
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+        </div>
       </div>
     </div>
   );
 }
 
 export default Product;
+
+function ProductItem() {
+  const [rating, setRating] = useState(2);
+
+  return (
+    <div className="product-item">
+      <div className="product-img-wrapper">
+        <img className="product-img" src="/images/product/product-1.jpg"></img>
+      </div>
+      <h4 className="product-title">FELET POWER CUSHION MENS RED</h4>
+      <div className="product-price">
+        <span className="product-price-before">RM999.99</span>
+        <span className="product-price-after">RM666.99</span>
+      </div>
+      <div className="star-rating">
+        <StarRatings
+          className="star"
+          starRatedColor={"hsl(38, 87%, 50%)"}
+          rating={2}
+          numberOfStars={5}
+          starDimension="15px"
+          starSpacing="0.2px"
+        />
+        <span>1.3k Sold</span>
+      </div>
+    </div>
+  );
+}
