b4768cd071855e27cddb602ac0b4fa2c217cf9bd
Wei Loong <nganweiloong@gmail.com>
2022-01-17 18:45:22 +0800

    added css to signup

--- src/Component/Pages/Signup.js
+++ src/Component/Pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { Animated } from "react-animated-css";
 
 function Signup() {
   const [fullname, setFullname] = useState("");
@@ -11,6 +12,7 @@ function Signup() {
   const [successMsg, setSuccessMsg] = useState("");
   const [loading, setLoading] = useState(false);
   const { signup, currentUser } = useAuth();
+  const navigate = useNavigate();
 
   async function handleSignup(e) {
     e.preventDefault();
@@ -21,41 +23,80 @@ function Signup() {
       setErrorMsg("");
       setLoading(true);
       await signup(email, password);
+      navigate("/");
     } catch {
-      setErrorMsg("Failed to create an error");
+      setErrorMsg("Failed to create an account!");
     }
     setLoading(false);
   }
   return (
-    <form onSubmit={handleSignup}>
-      {errorMsg && <h2>{errorMsg} </h2>}
-      current user => {currentUser?.email}
-      <h1>Sign Up</h1>
-      <h4>
-        {fullname}
-        {email}
-        {password}
-        {passwordConfirm}
-      </h4>
-      {/* <label>Full name</label>
-      <input onChange={(e) => setFullname(e.target.value)} type="text"></input> */}
-      <label>email</label>
-      <input onChange={(e) => setEmail(e.target.value)} type="email"></input>
-      <label>password</label>
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-      ></input>
-      <label>password confirm</label>
-      <input
-        onChange={(e) => setPasswordConfirm(e.target.value)}
-        type="password"
-      ></input>
-      Already got account ? Login in <Link to="/login">here</Link>
-      <button disabled={loading} className="btn-shop">
-        Sign up
-      </button>
-    </form>
+    <div className="login-background">
+      <div className="fixed-container">
+        <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+          <div className="login-form-container">
+            <form className="login-form" onSubmit={handleSignup}>
+              <h2 className="login-form-title">Sign Up</h2>
+              {errorMsg && <div className="error-msg">{errorMsg}</div>}
+              <div className="input-field">
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="Email"
+                ></input>
+              </div>
+              <div className="input-field">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                ></input>
+              </div>
+              <div className="input-field">
+                <input
+                  onChange={(e) => setPasswordConfirm(e.target.value)}
+                  type="password"
+                  placeholder="Password Confirmation"
+                ></input>
+              </div>
+              <button className="btn-shop">Sign up now</button>
+              <div className="form-forgot-password">Forgot password?</div>
+              <div>
+                Already have an account? <Link to="/login"> Login here</Link>
+              </div>
+            </form>
+          </div>
+        </Animated>
+      </div>
+    </div>
+    // <form onSubmit={handleSignup}>
+    //   {errorMsg && <h2>{errorMsg} </h2>}
+    //   current user => {currentUser?.email}
+    //   <h1>Sign Up</h1>
+    //   <h4>
+    //     {fullname}
+    //     {email}
+    //     {password}
+    //     {passwordConfirm}
+    //   </h4>
+    //   {/* <label>Full name</label>
+    //   <input onChange={(e) => setFullname(e.target.value)} type="text"></input> */}
+    //   <label>email</label>
+    //   <input onChange={(e) => setEmail(e.target.value)} type="email"></input>
+    //   <label>password</label>
+    //   <input
+    //     onChange={(e) => setPassword(e.target.value)}
+    //     type="password"
+    //   ></input>
+    //   <label>password confirm</label>
+    //   <input
+    //     onChange={(e) => setPasswordConfirm(e.target.value)}
+    //     type="password"
+    //   ></input>
+    //   Already got account ? Login in <Link to="/login">here</Link>
+    //   <button disabled={loading} className="btn-shop">
+    //     Sign up
+    //   </button>
+    // </form>
   );
 }
 
