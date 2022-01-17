00d7db4ae391acca10593d5dde4d0e0553468cdb
Wei Loong <nganweiloong@gmail.com>
2022-01-17 18:33:49 +0800

    added css to login page

--- src/Component/Pages/Login.js
+++ src/Component/Pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { Animated } from "react-animated-css";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -24,24 +25,37 @@ function Login() {
     setLoading(false);
   }
   return (
-    <form onSubmit={handleLogin}>
-      {errorMsg && <h1>{errorMsg}</h1>}
-      {currentUser?.email}
-      <h3>
-        current input
-        {email}
-        {password}
-      </h3>
-      <label>login</label>
-      <input onChange={(e) => setEmail(e.target.value)} type="email"></input>
-      <label>password</label>
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-      ></input>
-      <button className="btn-shop">login now</button>
-      No account? Sign up <Link to="/signup">here</Link>
-    </form>
+    <div className="login-background">
+      <div className="fixed-container">
+        <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+          <div className="login-form-container">
+            <form className="login-form" onSubmit={handleLogin}>
+              <h2 className="login-form-title">Login</h2>
+              {errorMsg && <div className="error-msg">{errorMsg}</div>}
+              <div className="input-field">
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="Email"
+                ></input>
+              </div>
+              <div className="input-field">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                ></input>
+              </div>
+              <button className="btn-shop">Login now</button>
+              <div className="form-forgot-password">Forgot password?</div>
+              <div>
+                No account? Sign up <Link to="/signup">here</Link>
+              </div>
+            </form>
+          </div>
+        </Animated>
+      </div>
+    </div>
   );
 }
 
